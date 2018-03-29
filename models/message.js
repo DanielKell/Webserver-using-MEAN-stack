62f9d4560d288b995a085113dc6b5eaf9f8d4b6a
DanielKell <danielkell.ubc@gmail.com>
2018-03-28 21:04:19 -0700

    Mongoose Setup & Schemas modelled

---
+++ models/message.js
@@ -0,0 +1,11 @@
+//Mongoose model for messages used on the app
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+const schema = new Schema({
+    content: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Message', schema);
